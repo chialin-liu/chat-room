f4d4ccf41c259e3eef739f44df5dbf794d04bead
liuchialin <charles.ee96@g2.nctu.edu.tw>
2019-06-13 21:52:35 +0800

    finished different users list

--- public/js/chat.js
+++ public/js/chat.js
@@ -9,9 +9,24 @@ const messageTemplate = document.querySelector("#message-template").innerHTML;
 const locationMessageTemplate = document.querySelector(
   "#location-message-template"
 ).innerHTML;
+const sidebarTemplate = document.querySelector("#sidebar-template").innerHTML;
 const { username, room } = Qs.parse(location.search, {
   ignoreQueryPrefix: true
 });
+const autoscroll = () => {
+  const $newMessage = $messages.lastElementChild;
+  const newMessageStyles = getComputedStyle($newMessage);
+  const newMessageMargin = parseInt(newMessageStyles.marginBottom);
+  const newMessageHeight = $newMessage.offsetHeight + newMessageMargin;
+
+  const visibleHeight = $messages.offsetHeight;
+  const containerHeight = $messages.scrollHeight;
+  const scrollOffset = $messages.scrollTop + visibleHeight;
+  if (containerHeight - newMessageHeight <= scrollOffset) {
+    $messages.scrollTop = $messages.scrollHeight;
+  }
+  console.log(newMessageMargin);
+};
 socket.on("message", message => {
   console.log(message);
   const html = Mustache.render(messageTemplate, {
@@ -20,6 +35,16 @@ socket.on("message", message => {
     createAt: moment(message.createAt).format("H:mm")
   });
   $messages.insertAdjacentHTML("beforeend", html);
+  autoscroll();
+});
+socket.on("roomData", ({ room, users }) => {
+  // console.log(room);
+  // console.log(users);
+  const html = Mustache.render(sidebarTemplate, {
+    room,
+    users
+  });
+  document.querySelector("#sidebar").innerHTML = html;
 });
 //retrieve the input
 $messageForm.addEventListener("submit", e => {
@@ -44,6 +69,7 @@ socket.on("locationMessage", message => {
     createAt: moment(message.createAt).format("H:mm")
   });
   $messages.insertAdjacentHTML("beforeend", html);
+  autoscroll();
 });
 //retrieve the location
 $sendLocationButton.addEventListener("click", () => {
--- src/index.js
+++ src/index.js
@@ -33,6 +33,10 @@ io.on("connection", socket => {
         "message",
         generateMessage("Admin", `${user.username} has joined in`)
       );
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room)
+    });
     callback();
   });
   socket.on("sendMessage", (message, callback) => {
@@ -62,6 +66,10 @@ io.on("connection", socket => {
         "message",
         generateMessage(`${user.username} user has left`)
       );
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room)
+      });
     }
   });
 });
